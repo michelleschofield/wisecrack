a8cd4fb61e539390147ac325c457fed10f234e53
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-02 17:25:31 +0000

    Values are retrieved from form when submitted #1

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-interface FormElements extends HTMLFormControlsCollection {
-  contains: HTMLInputElement;
-  type: HTMLInputElement;
-}
-
-const $form = document.querySelector('form') as HTMLFormElement;
-const $categories = document.querySelectorAll(
-  '[data-category]',
-) as NodeListOf<HTMLInputElement>;
-
+'use strict';
+const $form = document.querySelector('form');
+const $categories = document.querySelectorAll('[data-category]');
 if (!$form) throw new Error('$form query failed');
 if (!$categories) throw new Error('$categories query failed');
-
 $form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event: Event): void {
+function handleSubmit(event) {
   event.preventDefault();
   if (!$form) return;
-
-  const $formElements = $form.elements as FormElements;
-  const categoriesArray: string[] = [];
-
+  const $formElements = $form.elements;
+  const categoriesArray = [];
   $categories.forEach((checkbox) => {
     if (checkbox.checked) categoriesArray.push(checkbox.value);
   });
   const categories = categoriesArray.join(',');
-
   const formValues = {
     contains: $formElements.contains.value,
     type: $formElements.type.value,
